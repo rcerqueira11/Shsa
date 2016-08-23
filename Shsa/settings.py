ffc0e20d167f48ebeab8b56e8f47603353593035
Ricardo Cerqueira <raxs64@gmail.com>
2016-08-23 18:49:57 -0400

    modificado settings

--- Shsa/settings.py
+++ Shsa/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.registro',
+    'apps.rcs',
 )
 
 MIDDLEWARE_CLASSES = (
--- Shsa/urls.py
+++ Shsa/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from apps.registro import views
 
 urlpatterns = [
     # Examples:
