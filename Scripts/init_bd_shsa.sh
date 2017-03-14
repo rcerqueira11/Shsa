39c1aab8c537bb182f3b30c4af0789dd29c0008c
Ricardo Cerqueira <raxs64@gmail.com>
2017-03-14 18:49:55 -0400

    Agregado algunos fixtures y adelanto del registro

---
+++ Scripts/init_bd_shsa.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+export pg_user='postgres'
+export pg_password='123456'
+# export pg_password='postgres'
+export pg_db='shsa_db'
+export pg_host='127.0.0.1'
+
+echo "~~~~ Deleting database ~~~~"
+export PGPASSWORD=$pg_password
+dropdb -U$pg_user $pg_db
+
+echo -e "\n\n~~~~ Recreate database ~~~~"
+export PGPASSWORD=$pg_password
+createdb -U$pg_user -E'UTF-8' $pg_db
+
+
+python manage.py makemigrations
+python manage.py migrate 
+
+echo "Cargando Fixtures de Registro"
+
+python manage.py loaddata apps/registro/fixtures/0*
